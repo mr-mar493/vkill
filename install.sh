3c9dfd7cc175de5f9fc0caf2505f4dff3243904c
Alexey0109 <grudko2003@gmail.com>
2021-07-08 10:13:58 +0300

    Update. Added logs

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ echo Upgrading packages...
 apt update
 apt upgrade
 echo python install...
-apt install python3
+apt install python
 pip3 install vk_api
 echo done!
 sleep 2
+mkdir logs
 python3 main.py
